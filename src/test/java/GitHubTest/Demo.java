206791219bfd701e4142a62edd332c23d264fbbb
Admin <Admin@DESKTOP-KEV4HHF>
2022-01-24 20:05:30 +0530

    user1 second change

--- src/test/java/GitHubTest/Demo.java
+++ src/test/java/GitHubTest/Demo.java
@@ -7,9 +7,13 @@ public class Demo
 	@Test
 	public void demoTest()
 	{
-		System.out.println("This is my fist program");
+		System.out.println("This is user 1 fist program");
 		
 		System.out.println("This is user 2 first program");
+		
+		System.out.println("This is user 1 second program");
+		
+		
 	}
 
 }
