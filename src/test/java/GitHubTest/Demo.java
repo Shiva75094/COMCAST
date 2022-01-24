f355f58d217f185b845be464c1bc91224c910eff
Admin <Admin@DESKTOP-KEV4HHF>
2022-01-24 23:32:29 +0530

    Branch 1 tc_01

--- src/test/java/GitHubTest/Demo.java
+++ src/test/java/GitHubTest/Demo.java
@@ -10,23 +10,13 @@ public class Demo
 		System.out.println("This is user 1 fist program");
 		
 		System.out.println("This is user 2 first program");
-<<<<<<< HEAD
 		
-=======
-
->>>>>>> branch 'master' of https://github.com/Shiva75094/COMCAST.git
 		System.out.println("This is user 2 second program");
 
 		System.out.println("This is user 1 second program");
 		
-<<<<<<< HEAD
 		System.out.println("This is user 2 third program");
 		
-=======
-		System.out.println("This is user 1 third program");
-		
-
->>>>>>> branch 'master' of https://github.com/Shiva75094/COMCAST.git
 	}
 
 }
