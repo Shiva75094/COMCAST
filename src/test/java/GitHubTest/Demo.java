fcf73e2685f1a5d684048beee48d1fbf50d5bcef
Admin <Admin@DESKTOP-KEV4HHF>
2022-01-24 23:10:32 +0530

    user1 third change

--- src/test/java/GitHubTest/Demo.java
+++ src/test/java/GitHubTest/Demo.java
@@ -10,14 +10,14 @@ public class Demo
 		System.out.println("This is user 1 fist program");
 		
 		System.out.println("This is user 2 first program");
-		
-<<<<<<< HEAD
+
 		System.out.println("This is user 2 second program");
-=======
+
 		System.out.println("This is user 1 second program");
 		
+		System.out.println("This is user 1 third program");
 		
->>>>>>> branch 'master' of https://github.com/Shiva75094/COMCAST.git
+
 	}
 
 }
